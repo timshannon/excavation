7ce826d3455ba7e054905cc944164b7821e1b0dd
Tim Shannon <tim@townsourced.com>
2010-09-23 09:23:39 -0500

    Documentation changes

--- src/pandatest.py
+++ src/pandatest.py
@@ -5,5 +5,15 @@ class MyApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
  
+        # Load the environment model.
+        self.environ = self.loader.loadModel("/home/tshannon/workspace/excavation/data/models/levels/leveltest.egg")
+        # Reparent the model to render.
+        self.environ.reparentTo(self.render)
+        # Apply scale and position transforms on the model.
+        self.environ.setScale(0.25, 0.25, 0.25)
+        self.environ.setPos(-8, 42, 0)
+ 
+ 
 app = MyApp()
 app.run()
+
