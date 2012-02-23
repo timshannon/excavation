92932dc0974f5fb3b416a7b13564e50dff435386
Tim Shannon <tim@townsourced.com>
2012-02-23 16:43:46 -0600

    Added main executable to quick test accessing the necessary libraries with the new gopath setup.

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"excavation/engine/horde3d"
+	"fmt"
+	"github.com/banthar/Go-SDL/sdl"
+)
+
+func main() {
+	test := sdl.BUTTON_X2
+	test2 := horde3d.H3DEmitter_DelayF
+
+	fmt.Println("test:", test)
+	fmt.Println("test2:", test2)
+
+}
