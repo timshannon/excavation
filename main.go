f31881ffcc2e1887cc647b209d0fa3896b78ca7b
Tim Shannon <shannon.timothy@gmail.com>
2012-02-23 16:56:21 -0600

    More framework tests for how my libraries will be setup and consumed in go

--- main.go
+++ main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"excavation/engine/horde3d"
+	"excavation/engine"
 	"fmt"
-	"github.com/banthar/Go-SDL/sdl"
 )
 
 func main() {
-	test := sdl.BUTTON_X2
-	test2 := horde3d.H3DEmitter_DelayF
-
-	fmt.Println("test:", test)
-	fmt.Println("test2:", test2)
-
+	test := engine.Init()
+	fmt.Println("Test?: ", test)
 }
