22772c887f84125ec0b73e8595465d23fe42bc46
Tim Shannon <shannon.timothy@gmail.com>
2012-07-06 22:52:24 -0500

    Started filling out the general layout of the engine.  Got config files mostly in place

--- engine/engine.go
+++ engine/engine.go
@@ -1,42 +1,97 @@
 package engine
 
 import (
-	"excavation/engine/horde3d"
-	"fmt"
-	"github.com/banthar/Go-SDL/sdl"
+	"code.google.com/p/gohorde/horde3d"
+	"errors"
+	"github.com/jteeuwen/glfw"
 )
 
-func Init() bool {
-	var running bool = true
-	var cam horde3d.H3DNode = 0
-	var testRes horde3d.H3DRes = 1234
+const (
+	windowTitle = "Excavation"
+)
 
-	if sdl.Init(sdl.INIT_VIDEO) != 0 {
-		panic(sdl.GetError())
+//globally accessible camera and pipeline
+var Cam horde3d.H3DNode
+var pipeline horde3d.H3DRes
+var running bool
+
+func Init() error {
+
+	//load settings from config file
+	cfg, err := LoadConfig()
+	if err != nil {
+		return err
 	}
-	sdl.WM_SetCaption("Excavation", "test")
 
-	//set sdl video mode
-	if sdl.SetVideoMode(800, 600, 32, sdl.OPENGL) == nil {
-		panic(sdl.GetError())
+	if err = glfw.Init(); err != nil {
+		return err
 	}
 
-	horde3d.H3dInit()
-	fmt.Println("Version: ", horde3d.H3dGetVersionString())
-
-	for running == true {
-		switch event := sdl.PollEvent(); event.(type) {
-		case *sdl.QuitEvent:
-			running = false
-			break
-		}
-		horde3d.H3dRender(cam)
-		testRes = horde3d.H3dFindResource(horde3d.H3DResTypes_Material, "test")
-		horde3d.H3dIsResLoaded(testRes)
-		sdl.GL_SwapBuffers()
+	//TODO: Load from config
+	if err := glfw.OpenWindow(cfg.Int("WindowWidth"),
+		cfg.Int("WindowHeight"), 8, 8, 8, 8,
+		cfg.Int("WindowDepth"), 8,
+		cfg.Int("WindowMode")); err != nil {
+		return err
 	}
-	horde3d.H3dRelease()
-	sdl.Quit()
-	return true
+
+	//vsync from settings file
+	glfw.SetSwapInterval(1)
+	glfw.SetWindowTitle(windowTitle)
+
+	if !horde3d.Init() {
+		horde3d.DumpMessages()
+		return errors.New("Error starting Horde3D.  Check Horde3D_log.html for more information")
+	}
+
+	//load pipeline
+	//TODO: Resource manager
+	pipeline = horde3d.AddResource(horde3d.ResTypes_Pipeline, "pipelines/hdr.pipeline.xml", 0)
+	horde3d.LoadResourcesFromDisk("../content")
+
+	//add camera
+	Cam = horde3d.AddCameraNode(horde3d.RootNode, "Camera", pipeline)
+	glfw.SetWindowSizeCallback(onResize)
+	return nil
+
+}
+
+func StartMainLoop() {
+	running = true
+
+	for running {
+		//TODO: taskmanager
+		horde3d.Render(Cam)
+		horde3d.FinalizeFrame()
+		glfw.SwapBuffers()
+
+		//TODO: handle with input and tasks
+		running = glfw.Key(glfw.KeyEsc) == 0 &&
+			glfw.WindowParam(glfw.Opened) == 1
+
+	}
+
+	horde3d.Release()
+	glfw.Terminate()
+	glfw.CloseWindow()
+}
+
+func StopMainLoop() {
+	running = false
+}
+
+func onResize(w, h int) {
+	if h == 0 {
+		h = 1
+	}
+
+	horde3d.SetNodeParamI(Cam, horde3d.Camera_ViewportXI, 0)
+	horde3d.SetNodeParamI(Cam, horde3d.Camera_ViewportYI, 0)
+	horde3d.SetNodeParamI(Cam, horde3d.Camera_ViewportWidthI, w)
+	horde3d.SetNodeParamI(Cam, horde3d.Camera_ViewportHeightI, h)
+
+	//TODO: Set clip distance? Config?
+	horde3d.SetupCameraView(Cam, 45.0, float32(w)/float32(h), 0.1, 1000.0)
+	horde3d.ResizePipelineBuffers(pipeline, w, h)
 
 }
--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"excavation/engine"
-	"fmt"
 )
 
 func main() {
-	test := engine.Init()
-	fmt.Println("Test?: ", test)
+	if err := engine.Init(); err != nil {
+		panic("Error starting Excavation: " + err.Error())
+	}
+
+	engine.StartMainLoop()
+
 }
