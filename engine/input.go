49fabcf06f8646758537b6e3afefc6f3984fb550
Tim Shannon <shannon.timothy@gmail.com>
2012-07-16 23:20:10 -0500

    Started on more generic input handling.

--- engine/input.go
+++ engine/input.go
@@ -4,86 +4,26 @@ import (
 	"github.com/jteeuwen/glfw"
 )
 
-var (
-	pressEvents    map[int]KeyHandler
-	releaseEvents  map[int]KeyHandler
-	mButtonPress   map[int]MouseButtonHandler
-	mButtonRelease map[int]MouseButtonHandler
-)
+type InputHandler func(input Input)
 
-//Retyped from glfw in case I ever swap it out with a different lib
-// so I don't have references in my game code to external libraries, 
-// the engine should handle that
-type KeyHandler func(key int)
-type MousePosHandler func(x, y int)
-type MouseButtonHandler func(button int)
-type MouseWheelHandler func(delta int)
-
-func init() {
-	pressEvents = make(map[int]KeyHandler)
-	releaseEvents = make(map[int]KeyHandler)
+type Input struct {
+	Dev	Device 
+	State int
+	Position     int
+	AxisPosition float32
 }
 
-func keyCallBack(key int, state int) {
-	var function KeyHandler
-	var ok bool
 
-	if state == glfw.KeyPress {
-		function, ok = pressEvents[key]
-	} else if state == glfw.KeyRelease {
-		function, ok = releaseEvents[key]
-	}
+type Device struct {
+	DeviceType int
+	DeviceIndex int
+	Button int
+	Axis int
 
-	if ok {
-		function(key)
-	}
+//A named control is bound to a Input Type
+// Game code refers to "strafeLeft" and gets the input type in the call
+// to determine how to handle the event
+
+func BindInput(controlName string, function InputHandler) {
+
 }
-
-func mButtonCallBack(button int, state int) {
-	var function MouseButtonHandler
-	var ok bool
-
-	if state == glfw.KeyPress {
-		function, ok = mButtonPress[button]
-	} else if state == glfw.KeyRelease {
-		function, ok = mButtonRelease[button]
-	}
-
-	if ok {
-		function(button)
-	}
-}
-
-func BindKeyPress(key int, function KeyHandler) {
-	pressEvents[key] = function
-}
-
-func BindKeyRelease(key int, function KeyHandler) {
-	releaseEvents[key] = function
-}
-
-func BindMButtonPress(button int, function MouseButtonHandler) {
-	mButtonPress[button] = function
-}
-
-func BindMButtonRelease(button int, function MouseButtonHandler) {
-	mButtonRelease[button] = function
-}
-
-func MousePos() (int, int) {
-	return glfw.MousePos()
-}
-
-func MouseWheelPos() int {
-	return glfw.MouseWheel()
-}
-
-func BindMousePos(function MousePosHandler) {
-	glfw.SetMousePosCallback(glfw.MousePosHandler(function))
-}
-
-func BindMouseWheel(function MouseWheelHandler) {
-	glfw.SetMouseWheelCallback(glfw.MouseWheelHandler(function))
-}
-
-//TODO: Joystick Detection and handling
