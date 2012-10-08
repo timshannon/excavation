e8535410fddf911a991ca32cb47fde2f26a1038b
Tim Shannon <shannon.timothy@gmail.com>
2012-10-07 22:21:29 -0500

    More entity loading, missed a few files.

---
+++ entity/entityTypes.go
@@ -0,0 +1,20 @@
+package entity
+
+import (
+	"errors"
+	"strings"
+)
+
+//NewEntity creates a new entity of the type passed in via a string
+// this is so entities can be loaded from the xml scene file
+func NewEntity(typeName string) (Entity, error) {
+
+	//Big constantly changing switch for now, not sure of a
+	// better way to handle this for now.
+	switch strings.ToLower(typeName) {
+	case "player":
+		return new(Player), nil
+	}
+	return nil, errors.New("Entity of type " + typeName + " not found.")
+
+}
