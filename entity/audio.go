4755171b938dda5ba5bb85bc4cc6ee81f459cd6f
Tim Shannon <tim@townsourced.com>
2012-11-20 15:44:21 -0600

    Finished up rewriting the audio framework.  It's more simple now and handles queueing sources and priorities.  Needs to be tested of course.

--- entity/audio.go
+++ entity/audio.go
@@ -4,15 +4,17 @@ import (
 	"excavation/engine"
 )
 
-type AudioStatic struct {
+type Audio struct {
 	audio *engine.Audio
 }
 
-func (a *AudioStatic) Add(node *engine.Node, args EntityArgs) {
+func (a *Audio) Add(node *engine.Node, args EntityArgs) {
 	a.audio = engine.AddAudioNode(node, args.String("file"), args.Float("minDistance"),
 		args.Float("maxDistance"), 10)
 
 	a.audio.Load()
-	//a.audio.Play()
-	a.audio.Looping = args.Bool("loop")
+	//a.audio.Play() //TODO: entity triggers
+	a.audio.SetLooping(args.Bool("loop"))
+	//TODO: task to check distance and automatically start and stop audio based on
+	// distance from listener i.e 2xMaxDistance
 }
