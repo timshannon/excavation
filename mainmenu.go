3edcbcf401efc7b3aa959a464b909b2f7100a899
Tim Shannon <tim@townsourced.com>
2012-12-11 22:17:04 -0600

    Messing around with trying to get the gui to work properly.

--- mainmenu.go
+++ mainmenu.go
@@ -10,8 +10,9 @@ var mainMenu *engine.Gui
 func loadMainMenu() {
 
 	mainMenu = new(engine.Gui)
+	mainMenu.UseMouse = true
 	button := gui.MakeButton("newButton", "New Game", 5,
-		engine.NewScreenArea(.5, .5, .2, .5, engine.ScreenRelativeLeft))
+		engine.NewScreenArea(.5, .5, .2, .5, engine.ScreenRelativeAspect))
 	mainMenu.AddWidget(button)
 
 	engine.LoadGui(mainMenu)
