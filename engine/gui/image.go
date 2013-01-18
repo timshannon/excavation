1e9b762f94dcf427dec860f67c02fb2a14ba8879
Tim Shannon <shannon.timothy@gmail.com>
2013-01-18 16:36:36 -0600

    added simple image widget

---
+++ engine/gui/image.go
@@ -0,0 +1,28 @@
+package gui
+
+import (
+	"excavation/engine"
+)
+
+type Image struct {
+	name    string
+	Overlay *engine.Overlay
+}
+
+func MakeImage(name, imagePath string, dimensions *engine.ScreenArea) *Image {
+	newImage := new(Image)
+	newImage.Overlay = engine.NewOverlay(imagePath, engine.NewColor(255, 255, 255, 255), dimensions)
+	return newImage
+}
+
+func (i *Image) MouseArea() *engine.ScreenArea {
+	return i.Overlay.Dimensions
+}
+
+func (i *Image) Update() {
+	i.Overlay.Place()
+}
+
+func (i *Image) Hover()           { return }
+func (i *Image) Click(mouse int)  { return }
+func (i *Image) Scroll(delta int) { return }
